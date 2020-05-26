fd787ce7d6cf011ea4c1f7a3fa73634b762db90b
houjianping <houjianping@iyuedan.com>
2020-05-26 22:01:36 +0800

    up

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ import App from './App.vue'
 import voiceInputButton from './lib'
 
 Vue.use(voiceInputButton, {
-  appId: '',
-  apiKey: '',
-  apiSecret: ''
+  appId: '5eccc34b',
+  apiKey: '0aa4dcd2512bbbe86cdebf98fddca25c',
+  apiSecret: 'bbad948d40549a93d57f8b276c336d70'
 })
 
 new Vue({
