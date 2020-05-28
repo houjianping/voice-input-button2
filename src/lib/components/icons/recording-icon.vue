fc3c78c1c1ee06bc8d3328e88ab6d60a1c5763f3
houjianping <houjianping@iyuedan.com>
2020-05-28 20:51:09 +0800

    update

--- src/lib/components/icons/recording-icon.vue
+++ src/lib/components/icons/recording-icon.vue
@@ -1,16 +1,16 @@
 <!-- from https://codepen.io/aurer/pen/jEGbA -->
 <template>
   <div class="recording" title="recording…">
-    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="20px" height="20px" viewBox="0 0 20 20" style="enable-background:new 0 0 50 50;" xml:space="preserve">
-      <rect x="0" y="8" width="4" height="4" :fill="color">
+    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="15px" height="15px" viewBox="0 0 20 20" style="enable-background:new 0 0 50 50;" xml:space="preserve">
+      <rect x="3" y="8" width="4" height="4" :fill="color">
         <animate attributeName="height" attributeType="XML" values="4;20;4" begin="0s" dur="0.6s" repeatCount="indefinite" />
         <animate attributeName="y" attributeType="XML" values="8; 0; 8" begin="0s" dur="0.6s" repeatCount="indefinite" />
       </rect>
-      <rect x="7" y="8" width="4" height="4" :fill="color">
+      <rect x="10" y="8" width="4" height="4" :fill="color">
         <animate attributeName="height" attributeType="XML" values="4;20;4" begin="0.15s" dur="0.6s" repeatCount="indefinite" />
         <animate attributeName="y" attributeType="XML" values="8; 0; 8" begin="0.15s" dur="0.6s" repeatCount="indefinite" />
       </rect>
-      <rect x="14" y="8" width="4" height="4" :fill="color">
+      <rect x="17" y="8" width="4" height="4" :fill="color">
         <animate attributeName="height" attributeType="XML" values="4;20;4" begin="0.3s" dur="0.6s" repeatCount="indefinite" />
         <animate attributeName="y" attributeType="XML" values="8; 0; 8" begin="0.3s" dur="0.6s" repeatCount="indefinite" />
       </rect>
@@ -33,7 +33,7 @@ export default {
 <style lang="scss" scoped>
   .recording{
     width: 20px;
-    height: 30px;
+    height: 20px;
     margin: auto;
   }
 </style>
